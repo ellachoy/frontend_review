c8a8752d4cf83c4e7cbbca32e44ca9d94e7a964e
ella choy <egal21de@gmail.com>
2022-02-04 22:40:29 +0100

    js_func

--- main.js
+++ main.js
@@ -1,29 +1,46 @@
 console.log("hi")
 
+function LinksSetColor(color) {
+    var alist =document.querySelectorAll('a');
+       var i =0;
+       while(i < alist.length){
+           alist[i].style.color = color;
+           i = i + 1;
+       }
+}
+function setColor(color){
+    target.style.color =color; 
+}
 function nightDayHandler(self){
     var target = document.querySelector('body')
     if (self.value === 'night'){
        target.style.backgroundColor ='black';
-      target.style.color ='white';
+       setColor('white')
+    //   target.style.color ='white';
       self.value = 'day';
+
+      LinksSetColor('red');
        
-       var alist =document.querySelectorAll('a');
-       var i =0;
-       while(i < alist.length){
-           alist[i].style.color = 'red';
-           i = i + 1;
-       }
+    //    var alist =document.querySelectorAll('a');
+    //    var i =0;
+    //    while(i < alist.length){
+    //        alist[i].style.color = 'red';
+    //        i = i + 1;
+    //    }
     }
     else {
        target.style.backgroundColor='white';
-       target.style.color='black';
+       setColor('black')
+    //    target.style.color='black';
        self.value = 'night'; 
+
+       LinksSetColor('green')
        
-       var alist =document.querySelectorAll('a');
-       var i =0;
-       while(i < alist.length){
-           alist[i].style.color = 'green';
-           i = i + 1;
-       }
+    //    var alist =document.querySelectorAll('a');
+    //    var i =0;
+    //    while(i < alist.length){
+    //        alist[i].style.color = 'green';
+    //        i = i + 1;
+    //    }
    }
 }
