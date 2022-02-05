ea98fa8ccb84953ad6f41581f759513d0a3eed72
ella choy <egal21de@gmail.com>
2022-02-05 21:13:28 +0100

    obj_methode

--- main.js
+++ main.js
@@ -1,25 +1,43 @@
 console.log("hi")
 var target = document.querySelector('body')
-function LinksSetColor(color) {
-    var alist =document.querySelectorAll('a');
-       var i =0;
-       while(i < alist.length){
-           alist[i].style.color = color;
-           i = i + 1;
-       }
+// function LinksSetColor(color) {
+//     var alist =document.querySelectorAll('a');
+//        var i =0;
+//        while(i < alist.length){
+//            alist[i].style.color = color;
+//            i = i + 1;
+//        }
+// }
+var Links = {
+    setColor:function(color){
+      var alist = document.querySelectorAll('a');
+      var i = 0;
+      while(i < alist.length){
+        alist[i].style.color = color;
+        i = i + 1;
+      }
+    }
+  }
+var Body ={
+    setColor:function(color){
+        document.querySelector('Body').style.color = color;
+    },
+    setBackgroundColor:function(color){
+        document.querySelector('Body').style.backgroundColor = color;
+    }
 }
-function BodySetColor(color){
-    target.style.color =color; 
-}
-function BodySetBackgroundColor(color){
-    target.style.backgroundColor =color; 
-}
+// function BodySetColor(color){
+//     target.style.color =color; 
+// }
+// function BodySetBackgroundColor(color){
+//     target.style.backgroundColor =color; 
+// }
 function nightDayHandler(self){
     // var target = document.querySelector('body')
     if (self.value === 'night'){
     //    target.style.backgroundColor ='black';
-    BodySetBackgroundColor('black')
-       BodySetColor('white')
+    Body.setBackgroundColor('black')
+       Body.setColor('white')
     //   target.style.color ='white';
       self.value = 'day';
 
@@ -34,8 +52,8 @@ function nightDayHandler(self){
     }
     else {
     //    target.style.backgroundColor='white';
-    BodySetBackgroundColor('white')
-       BodySetColor('black')
+    Body.setBackgroundColor('white')
+       Body.setColor('black')
     //    target.style.color='black';
        self.value = 'night'; 
 
